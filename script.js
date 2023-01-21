1486128b0784b99c826b8f8289f3916637ab3204
Jarrett <contact@jarrettperkins.me>
2023-01-21 11:31:00 -0600

    Added buttons and basic formatting for calculator

--- script.js
+++ script.js
@@ -34,18 +34,6 @@ const divide = function(input) {
     return sum;
 };
 
-const power = function(input) {
-    return input[0] ** input[1];
-};
-
-const factorial = function(input) {
-	let sum = 1;
-    for (let i = input; i > 0; i--) {
-        sum *= i;
-    }
-    return sum;
-};
-
 const operate = function(input,operand) {
     // ...
 };
