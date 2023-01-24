fbc28b2e33cecf7d094c3a4fca2b4b63f20fde1f
Jarrett <contact@jarrettperkins.me>
2023-01-23 19:35:34 -0600

    created operate function

--- script.js
+++ script.js
@@ -34,6 +34,28 @@ const divide = function(input) {
     return sum;
 };
 
-const operate = function(input,operand) {
-    // ...
+const operate = function(expression) {
+    input[0] = expression[0];
+    input[1] = expression[2];
+    
+    switch (true) {
+        case expression[1] == "+":
+            output = add(input);
+            break;
+        case expression[1] == "-":
+            output = subtract(input);
+            break;
+        case expression[1] == "*":
+            output = multiply(input);
+            break;
+        case expression[1] == "/":
+            output = divide(input);
+            break;
+    }
+    return output;
 };
+
+let displayValue = 0;
+let expression = [];
+let output = 0;
+let input = [];
